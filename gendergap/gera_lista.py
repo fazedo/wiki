62c7fca5adb7f2334f7ff715786b740d85c231fa
Fabio Azevedo <fazedo@gmail.com>
2018-05-16 11:43:00 +0000

    ajuste para filtrar usuários conhecicos

--- gendergap/gera_lista.py
+++ gendergap/gera_lista.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#Todo: remover bots com RonaldB, incluir lista de usuário sem gênero definido: JMGM
+#To do: remover bots com RonaldB, incluir lista de usuário sem gênero definido: JMGM
 
 from __future__ import division
 
@@ -17,6 +17,12 @@ ifile = open('lista_editores_ativos.json')
 lista = json.loads(ifile.read())
 ifile.close()
 
+lista = sorted(lista, key = lambda f:-f[2])
+
+lista_bots = ['RonaldB']
+lista_male = []   #'Umberto Bottura']
+lista_female = [] #'JMGM']
+
 male = 0
 female = 0
 total = 0
@@ -24,23 +30,26 @@ malecount = 0
 femalecount = 0
 totalcount = 0
 
+lista_sem_genero=''
 
-limiar = 10000
-limiar_max = 100000
+limiar = 1
+limiar_max = 100
 
-verbose = False 
-verbose = True
+verbose = False
+#verbose = True
 for nome,  genero, contagem in lista:
-	if contagem >= limiar and contagem <=limiar_max:
-		if genero == 'male':
+	if contagem >= limiar and contagem <=limiar_max and not nome in lista_bots:
+		if genero == 'male' or nome in lista_male:
 			male = male + 1
 			malecount = malecount + contagem
-		elif genero == 'female':
+		elif genero == 'female' or nome in lista_female:
 			female = female + 1
 			femalecount = femalecount + contagem
+			print nome
 		else: 		#neutro/nao declarou
 			if verbose:
 				print nome, genero, contagem
+				lista_sem_genero = lista_sem_genero + nome + '\n'
 		total = total + 1
 		totalcount = totalcount + contagem
 
@@ -53,3 +62,4 @@ print 'Declarados count:', malecount + femalecount, 'seja: ', 100*(malecount + f
 print 'Feminino count:', 100*femalecount/(femalecount+malecount)
 print male, female, total-female-male, total
 
+#open('lista_sem_genero.txt','w').write(lista_sem_genero)
